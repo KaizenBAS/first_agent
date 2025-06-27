21455f5c2b19cbb90957eeb884e2d9a602925c0d
bilal <basgym3@gmail.com>
2025-06-28 01:14:40 +0500

    firstAgent

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+from dotenv import load_dotenv
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
+from agents.run import RunConfig
+import asyncio
+
+# Load the environment variables from the .env file
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+#print(gemini_api_key)
+
+
+#Reference:  https://ai.google.dev/gemini-api/docs/openai   
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel( 
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+
+
+async def main():
+    agent = Agent(
+        name='Bill jobs',
+        instructions='You are a Programming Assistant.'
+    )
+
+
+
+    while True:
+        prompt = input("ask question... \n")
+        if prompt.lower() in {'exit', 'quit'}:
+            print('exiting...')
+            break
+
+        # Run with full conversation history
+        result = await Runner.run(agent, prompt, run_config=config)
+
+        # Add assistant reply to history
+        agent_response = result.final_output
+
+        # Print reply
+        print(f"🤖 Agent: {agent_response}\n")
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
